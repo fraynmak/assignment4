fc9f62870bd40956dcf59b9b06cdc1bd5302520c
apple <apple@apples-MacBook-Pro.local>
2017-12-20 12:56:33 +0800

    Completed instructions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
   // Log message to console to verify jQuery has loaded
   console.log('jquery is loaded');
-} )
+} );
+
+$(".readmore").click(showThisOnClick);
+function showThisOnClick() {
+	console.log('clicked');
+	$(".readmore").hide()
+	$("#show-this-on-click").show()
+	$(".readless").show()
+} ;
+
+$(".readless").click(hideThisOnClick);
+function hideThisOnClick() {
+	$(".readless").slideUp()
+	$("#show-this-on-click").hide()
+	$(".readmore").show()
+} ;
+
+$(".learnmore").click(learnMore);
+function learnMore() {
+	$("#learnmoretext").slideDown()
+	$(".learnmore").hide()
+} ;
